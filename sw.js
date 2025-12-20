5517c34955b5cb4bb5ac35c592f99143ec905caa
samstovall5-png <samstovall5@gmail.com>
2025-12-20 12:27:08 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "clue-sheet-blue-v1";
+const ASSETS = [
+  "./",
+  "./clue-sheet.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
