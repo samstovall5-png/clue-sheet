3fcf2f8b5bc1ed2d359952e05d66e1331c349605
samstovall5-png <samstovall5@gmail.com>
2025-12-20 14:32:34 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-const CACHE = "clue-sheet-blue-v7";
-const ASSETS = [
-  "./",
-  "./clue-sheet.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
-});
+const CACHE="detective-notes-v8";
+const ASSETS=["./","./clue-sheet.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
